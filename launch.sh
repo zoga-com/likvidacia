aba369add79e82ff2ade05d613340e6435e3fa3a
dima <zoga.debian@gmail.com>
2023-12-09 23:40:31 +0300

    ридми

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ ! -v LIQUIDATOR_TELEGRAM_TOKEN ]]; then
-    echo "LIQUIDATOR_TELEGRAM_TOKEN не указана."
+    echo "Переменная окружения LIQUIDATOR_TELEGRAM_TOKEN не указана."
 elif [[ -z "$LIQUIDATOR_TELEGRAM_TOKEN" ]]; then
-    echo "LIQUIDATOR_TELEGRAM_TOKEN не указана."
+    echo "Переменная окружения LIQUIDATOR_TELEGRAM_TOKEN не указана."
 else
     echo "Запуск бота с токеном: $LIQUIDATOR_TELEGRAM_TOKEN"
     cd build/
